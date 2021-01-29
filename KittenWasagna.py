b5518e1ab1ab8595e1e0c7a4145394bf116464fb
Mauro Strandberg <mauro.strandberg@gmail.com>
2021-01-29 22:40:37 +0100

    Uploaded the whole project at once

---
+++ KittenWasagna.py
@@ -0,0 +1,11 @@
+import wave
+from gtts import gTTS
+import os
+
+
+def Kittenwasagna():
+ os.system("start KittenWasagnaByLacucho.mp3")
+ sleep(4.1)
+ os.system("taskkill /IM Music.UI.exe /F")
+
+Kittenwasagna()
